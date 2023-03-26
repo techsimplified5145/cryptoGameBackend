85b4ce5e9d456e9b3c02c60d387699021df31e18
Maaz Khan <85396969+maazkhansw5145@users.noreply.github.com>
2023-03-26 07:57:17 +0500

    port updated

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 7001;
 
 const cors = require("cors");
 app.use(cors());
